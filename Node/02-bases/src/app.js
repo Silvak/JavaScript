423171e93d468b119d8a3123b656ab44658942f5
silvak <silvak.jeg@gmail.com>
2025-05-15 19:58:57 -0400

    add

--- Node/02-bases/src/app.js
+++ Node/02-bases/src/app.js
@@ -1,13 +1,30 @@
-const { getPokemonById } = require("./js-foundation/06-promises")
 
+const { buildLogger } = require('./plugins')
+
+const logger = buildLogger('app.js')
+
+logger.log("hola ")
+
+
+
+
+
+
+
+
+
+
+
+
+//Promises
+/*
+const { getPokemonById } = require("./js-foundation/06-promises")
 
 getPokemonById(4)
   .then((pokemon) => console.log({ pokemon }))
   .catch((error) => console.log(error))
   .finally(() => console.log("finalmente"))
-
-
-
+*/
 
 //const { emailTemplate } = require("./js-foundation/01-template");
 //require("./js-foundation/02-destructuring");
